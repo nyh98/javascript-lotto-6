1ccfd4cdb0d5865bbb5f70a8c3e6ea3d7bb745c9
nyh98 <nsi3358@naver.com>
2023-12-02 20:32:52 +0900

    당첨번호 입력, 예외 구현

--- src/Lotto/domain/InputView.js
+++ src/Lotto/domain/InputView.js
@@ -3,20 +3,35 @@ import MESSAGES from './Messages.js';
 
 const INPUT_VIEW = Object.freeze({
   async buyLotto() {
-    const MONEY = await Console.readLineAsync(MESSAGES.buyLotto);
+    const MONEY = Number(await Console.readLineAsync(MESSAGES.inputMoney));
     this.validateMoney(MONEY);
     return MONEY;
+  },
+
+  async winNumber() {
+    const WIN_NUMBER = await Console.readLineAsync(MESSAGES.inputWinNumber);
+    this.validateWinNumber(WIN_NUMBER);
+    const SPLIT_NUMBER = WIN_NUMBER.split(',');
   },
 
   validateMoney(money) {
     const notNumber = /[^\d]/g;
 
     if (money % 1000 !== 0) {
-      throw new Error(MESSAGES.notOneThousand);
+      throw new Error(MESSAGES.invalidMoney);
     }
 
     if (notNumber.test(money)) {
       throw new Error(MESSAGES.notNumber);
+    }
+  },
+
+  validateWinNumber(winNumber) {
+    const validNumber =
+      /^(([1-9]|[1-3][0-9]|4[0-5]),){5}([1-9]|[1-3][0-9]|4[0-5])$/g;
+
+    if (!validNumber.test(winNumber)) {
+      throw new Error(MESSAGES.invalidWinNumber);
     }
   },
 });
--- src/Lotto/domain/Messages.js
+++ src/Lotto/domain/Messages.js
@@ -1,7 +1,9 @@
 const MESSAGES = Object.freeze({
-  buyLotto: '구입금액을 입력해 주세요. \n',
-  notOneThousand: '1000단위로 입력해 주세요',
-  notNumber: '숫자만 입력해 주세요',
+  inputMoney: '구입금액을 입력해 주세요.\n',
+  inputWinNumber: '당첨 번호를 입력해 주세요.\n',
+  invalidWinNumber: '유효하지않은 형식 입니다 ex) 1,2,3,4,5,6',
+  invalidMoney: '유효하지 않은 형식입니다 1000단위로 입력해 주세요',
+  notNumber: '특수문자 또는 음수값입니다',
 });
 
 export default MESSAGES;
